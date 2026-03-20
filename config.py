188a1682905bdf514746beee9186c0e99d8f5022
t5nn8ggs6s-beep <t5nn8ggs6s@privaterelay.appleid.com>
2026-03-20 22:18:14 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BOT_TOKEN = os.getenv("8705224805:AAGGR67NGUKBFL719Y4YZ2Ydh_Es8qarq8A")
+CHANNEL = "@happchannel"  # канал для проверки подписки
+
+PRICES = {
+    "week": 100,
+    "month": 350,
+    "year": 560
+}
+
+ADMIN_ID = 8146320391  # твой Telegram ID
